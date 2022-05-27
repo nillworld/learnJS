21bb2b514513b12acecb8e26e28396332816bc24
nill <wwaosldk@gmail.com>
2022-05-27 15:05:48 +0900

    reduce

--- functionalProgramming/iterable-reduce.js
+++ functionalProgramming/iterable-reduce.js
@@ -1,13 +1,16 @@
 const nums = [1, 2, 3, 4, 5];
 let total = 0;
+
+// for문으로 모든 인자 더하기
 for (const n of nums) {
   total = total + n;
 }
-
 console.log(total); // 15
 
-// 여기서 초기에 들어오는 acc는 시작 값
 const reduce = (f, acc, iter) => {
+  // 여기서 초기에 들어오는 acc는 시작 값
+  // * acc란 누산기(accumulator)로 cpu에서 중간 산술 논리 장치 결과가 저장되는 레지스트리.
+
   // js에서 사용하는 reduce는 acc가 optional이고, 이를 위해 다음과 같이 작성된다.
   if (!iter) {
     iter = acc[Symbol.iterator]();
@@ -18,7 +21,7 @@ const reduce = (f, acc, iter) => {
     acc = f(acc, a);
   }
   return acc;
-}; // acc는 누적 값
+};
 
 const add = (a, b) => a + b;
 
@@ -27,3 +30,6 @@ console.log(reduce(add, 0, nums)); // 15
 console.log(add(add(add(add(add(0, 1), 2), 3), 4), 5)); //15
 
 console.log(reduce(add, nums)); // 15 (acc가 optional일때)
+
+// Array.prototype.reduce()
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce#%EB%AA%85%EC%84%B8
