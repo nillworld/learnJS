14ed5b242c7e39862aa9a7b92e12f4cb391c78fa
nill <wwaosldk@gmail.com>
2022-06-02 08:52:28 +0900

    iterable check

---
+++ functionalProgramming/iterable-check.js
@@ -0,0 +1,47 @@
+// let arr = [1, 2, 3];
+// let map = new Map([
+//   ["a", 1],
+//   ["b", 2],
+//   ["c", 3],
+// ]);
+
+// // console.log(...arr, ...map);
+
+// arr[Symbol.iterator]();
+
+// const iterable_custom = {
+//   [Symbol.iterator]() {
+//     let i = 3;
+//     return {
+//       next() {
+//         return i === 0 ? { done: true } : { value: i--, done: false };
+//       },
+
+//       /*
+// 				iterator가 자기 자신을 반환하는 Symbol.iterator 메소드를 가지고 있을 때,
+// 				이를 '잘 구현된 iterator'(well formed iterator)라고 한다.
+// 			*/
+//       [Symbol.iterator]() {
+//         return this;
+//       },
+//     };
+//   },
+// };
+
+// for (const a of iterable_custom) console.log(a);
+
+const myiter = {
+  [Symbol.iterator]() {
+    let a = 0;
+    return {
+      next() {
+        return a > 10 ? { done: true } : { value: a++, done: false };
+      },
+      [Symbol.iterator]() {
+        return this;
+      },
+    };
+  },
+};
+
+for (const a of myiter) console.log(a);
