06be24c8bf832fc403f33f28955246a9dd45b314
nill <wwaosldk@gmail.com>
2022-06-10 15:01:04 +0900

    go - pipe

---
+++ functionalProgramming/iterable/go_pipe.js
@@ -0,0 +1,34 @@
+const reduce = (f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
+  return acc;
+};
+
+const add = (a, b) => a + b;
+
+const go = (...args) => reduce((a, f) => f(a), args);
+const pipe =
+  (f, ...fs) =>
+  (...as) =>
+    go(f(...as), ...fs);
+
+console.log(
+  go(
+    add(0, 1),
+    (a) => a + 10,
+    (a) => a + 100
+  )
+); //111
+
+const f = pipe(
+  (a, b) => a + b,
+  (a) => a + 10,
+  (a) => a + 100
+);
+
+console.log(f(0, 1)); // 111
---
+++ functionalProgramming/iterable/test.js
@@ -0,0 +1,15 @@
+const reduce = (f, acc, iter) => {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const a of iter) {
+    acc = f(acc, a);
+  }
+  return acc;
+};
+
+const add = (a, b) => a + b;
+let test = (...a) => reduce(add, a);
+
+console.log(test(1, 2, 3, 4));
