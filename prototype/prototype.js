26c03cea5ed1b380902b347b41c420126f1734ec
nill <wwaosldk@gmail.com>
2022-06-22 16:03:33 +0900

    prototype

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,3 +1,39 @@
+function Person() {}
+
+let nill = new Person(); // Person이라는 생성자 함수로 nill이라는 인스턴스 생성.
+let berry = new Person();
+console.log(Person.prototype.constructor);
+console.log(Person.prototype.constructor.prototype);
+console.log(Person.prototype.constructor.prototype.constructor);
+console.log(Person.prototype.constructor.prototype.constructor === Person.prototype.constructor);
+console.log(nill);
+console.log(nill.constructor);
+console.log(Person.prototype.constructor.prototype.constructor === nill.constructor);
+
+/* 
+
+prototype은 상위 객체의 상속처럼 보이지만, 실제로는 필요할때만 찾는 참조이다.
+
+*/
+
+Person.prototype.test = () => "test 속성.";
+console.log(nill.test());
+Person.prototype.test = () => "test 속성 바꿈.";
+console.log(nill.test());
+nill.test = () => "객체를 통해 test 속성 바꿈.";
+console.log(nill.test());
+console.log(berry.test());
+Person.prototype.test = () => "test 속성 생성자로 다시 바꾸면?";
+console.log(nill.test());
+console.log(berry.test());
+console.log(nill.__proto__); // '__proto__': 바로 위 객체 프로토타입을 가져 옴. (비표준 속성)
+console.log(Array.prototype.__proto__); // 상위 객체(Object) 프로토타입
+console.log(Object.prototype.__proto__); // 상위 객체 없음
+Object.prototype.top_prototype = "this prototype is from Object";
+console.log(nill.top_prototype);
+
+////////////////////////////////////////////
+
 console.log("Object property names", Object.getOwnPropertyNames(Object));
 console.log("Object.prototype property names", Object.getOwnPropertyNames(Object.prototype));
 console.log("Array property names", Object.getOwnPropertyNames(Array));
