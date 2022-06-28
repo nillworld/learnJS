15dd321832efb280e24792fb87133ec1bdcbb6a7
nill <wwaosldk@gmail.com>
2022-06-28 17:07:19 +0900

    spread_operator_test

--- functionalProgramming/iterable/spread_operator_test.js
+++ functionalProgramming/iterable/spread_operator_test.js
@@ -4,3 +4,10 @@ const test = (a, ...b) => {
 };
 
 test(4, 2, 1, 5, "2,4", "e");
+
+const test2 = (a, f) => {
+  console.log(f(a));
+};
+
+test2(10, (n) => n * 10); // 100
+test2((n) => n * 10, 10); // f is not a function
