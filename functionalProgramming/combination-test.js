157b226b881e57f36f1ad03c4d09184e8bb44878
nill <wwaosldk@gmail.com>
2022-07-24 00:04:50 +0900

    permutation function

--- functionalProgramming/combination-test.js
+++ functionalProgramming/combination-test.js
@@ -14,3 +14,26 @@ const getCombination = (arr, n) => {
 };
 
 console.log(getCombination([1, 2, 3], 2));
+
+// getCombination(n) = [...getCombination(n-1), a]
+
+// ===========================================
+
+const getPermutation = (arr, n) => {
+  const result = [];
+
+  if (n === 1) return arr.map((el) => [el]);
+
+  // getPermutation(n)=[[a, ...getPermutation(n-1)],[b, ...getPermutation(n-1)],[...] ...]
+  arr.forEach((a, i, array) => {
+    const copyArr = [...array];
+    copyArr.splice(i, 1);
+    const subPermutation = getPermutation(copyArr, n - 1);
+    const addPermutation = subPermutation.map((el) => [a, ...el]);
+    result.push(...addPermutation);
+  });
+
+  return result;
+};
+
+console.log(getPermutation([1, 2, 3], 2));
