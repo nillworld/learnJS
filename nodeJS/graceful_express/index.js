ef25c15bf335f35ae4301fe25176a2ff67eec34f
erp <tobeerp@tobesoft.com>
2022-11-15 17:25:43 +0900

    process signal test

--- nodeJS/graceful_express/index.js
+++ nodeJS/graceful_express/index.js
@@ -4,11 +4,13 @@ const fs = require("fs");
 const app = express();
 
 app.get("/", (req, res) => {
-  fs.cpSync("test1", "test4", {
-    recursive: true,
-    force: true,
-  });
-  return res.json({ ping: true });
+  // fs.cpSync("test1", "test4", {
+  //   recursive: true,
+  //   force: true,
+  // });
+  res.json({ ping: true, date: "HI" });
+  // const date = new Date();
+  // setTimeout(() => res.json({ ping: true, date: date }), 5000);
 });
 
 const server = app.listen(3000, () => console.log("Running…"));
@@ -18,13 +20,39 @@ setInterval(
   1000
 );
 
-process.on("SIGTERM", shutDown);
-process.on("SIGINT", shutDown);
-
 let connections = [];
+let count = 0;
+const test1 = () => {
+  if (count === 0) {
+    console.log("Connect 끊는 중...");
+    count += 1;
+    setTimeout(() => {
+      console.log("기달렸다가 강제 끊음");
+      process.exit(0);
+    }, 10000);
+  } else {
+    console.log("Connect 강제 끊기");
+    process.exit(0);
+  }
+};
+
+const test2 = () => {
+  console.log("zzzzzz");
+};
+
+const test3 = () => {
+  console.log("333333");
+};
+
+process.on("SIGTERM", test2);
+process.on("SIGTERM", test3);
+process.on("SIGINT", test1);
+process.on("SIGKILL", test3);
+// process.on("SIGINT", shutDown);
 
 server.on("connection", (connection) => {
   connections.push(connection);
+  console.log(connections);
   connection.on("close", () => (connections = connections.filter((curr) => curr !== connection)));
 });
 
@@ -41,5 +69,5 @@ function shutDown() {
   }, 10000);
 
   connections.forEach((curr) => curr.end());
-  setTimeout(() => connections.forEach((curr) => curr.destroy()), 5000);
+  setTimeout(() => connections.forEach((curr) => curr.destroy()), 10000);
 }
